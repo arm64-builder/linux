f9953ec47d969286553d2d492beea6c3894a229d
Vldly <92926213+vldly@users.noreply.github.com>
2022-11-21 17:19:26 +0200

    dt-bindings: clock: qcom: add bindings for MSM8953 APCC

---
+++ include/dt-bindings/clock/qcom,apcc-msm8953.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+
+#ifndef _DT_BINDINGS_CLK_MSM_APCC_8953_H
+#define _DT_BINDINGS_CLK_MSM_APCC_8953_H
+
+/* Clocks */
+#define APCC_CPU0_CLK		0
+#define APCC_CPU4_CLK		1
+#define APCC_CCI_CLK		2
+#define APCC_CPU0_PLL		3
+#define APCC_CPU4_PLL		4
+#define APCC_CCI_PLL		5
+#define APCC_NUM_CLOCKS		6
+
+#endif /* _DT_BINDINGS_CLK_MSM_APCC_8953_H */
